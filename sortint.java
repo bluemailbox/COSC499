0299f537da08dc370dba48b471f5d029c19df942
bluemailbox <77300788+bluemailbox@users.noreply.github.com>
2022-09-14 08:43:02 -0700

    Added fix for Junit testing, test will execute now

--- sortint.java
+++ sortint.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class sortint {
-    public static int sortInt() {
+    public int sortint(int a, int b, int c) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter 3 numbers");
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
-        int num3 = in.nextInt();
+        System.out.println("Here are 3 numbers: " + a + " " + b + " " + c);
+        int num1 = a;
+        int num2 = b;
+        int num3 = c;
         if(num1 > num2 && num1 > num3 && num2 > num3 ) {
             System.out.println("Order ascending: " + num3 + num2 + num1);
             return 0;
@@ -35,9 +36,7 @@ public class sortint {
         }
         
     }
-    public static void main(String[] args) {
-        sortInt();
-      }
       
 }
+
 
--- sortintTest.java
+++ sortintTest.java
@@ -1,11 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class sortintTest {
     @Test
     public void testSort() {
         sortint mytest = new sortint();
-        int result = mytest.sortInt();
-        assertEquals("0",result);
+        int result = mytest.sortint(1,2,3);
+        assertEquals(0,result);
     }
-
-    
 }
+
